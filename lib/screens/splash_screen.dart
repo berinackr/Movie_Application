3534bdc909282a345f6bf76bbb0c0f92f8b25258
berinackr <bernacakir888@gmail.com>
2024-02-20 14:17:12 +0300

    splash screen is created

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+//import 'package:movie_application/screens/home_screen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 2),
+      () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => //HomeScreen(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Splash Screen'),
+      ),
+    );
+  }
+}
